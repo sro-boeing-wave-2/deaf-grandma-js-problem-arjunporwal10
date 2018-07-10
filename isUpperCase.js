cf2ccba852cf3eefd42887e26f0912364e22ac9c
arjun <arjunporwal10@gmail.com>
2018-07-10 10:49:29 +0530

    lint and test case passed

--- isUpperCase.js
+++ isUpperCase.js
@@ -4,7 +4,7 @@
 
 /**
  * Checks if the string is in uppercase
- * @param {string} s The string to be checked
+ * @param {string} s The strifng to be checked
  * @returns {boolean}
  */
 const isUppercase = (s) => {
